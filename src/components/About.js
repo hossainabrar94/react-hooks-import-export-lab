09bd791c105c3db453236dbbde47ced5b2f09074
Abrar <hossainabrar@aol.com>
2024-02-27 20:02:21 -0500

    completed

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-function About() {
+function About(info) {
   return (
     <div id="about">
       <h2>About Me</h2>
       <p>I Made This</p>
-      <img src={image} alt="I made this" />
+      <img src={info.image} alt="I made this" />
     </div>
   );
 }
+
+export default About
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-function Home() {
+function Home(info) {
   return (
     <div id="home">
       <h1>
-        {username} is a Web Developer from {city}
+        {info.username} is a Web Developer from {info.city}
       </h1>
     </div>
   );
 }
+
+export default Home
